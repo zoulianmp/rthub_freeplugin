dfb64f40b0b01736830a22d657157ef6aaed4677
zoulian <zoulian@med.uestc.edu.cn>
2018-04-15 16:36:20 +0800

    add free plugins
    Signed-off-by: zoulian <zoulian@med.uestc.edu.cn>

---
+++ web_one2many_kanban/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2016 Serpent Consulting Services Pvt. Ltd
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Web One2many Kanban",
+    "version": "11.0.1.0.0",
+    "license": "AGPL-3",
+    "sequence": 6,
+    "author": "Serpent Consulting Services Pvt. Ltd.",
+    "maintainer": "Serpent Consulting Services Pvt. Ltd.",
+    "website": "http://www.serpentcs.com",
+    'summary': 'Display one2many widget as kanban',
+    "depends": [
+        "web",
+    ],
+    "data": [
+        "view/templates.xml",
+    ],
+    "images": ["static/description/o2mKanban.png"],
+    "installable": True,
+    "application": True,
+}
